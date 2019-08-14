8dfda6d8880a299c62f8c49458b114032b5a8807
Joshua Bohde <bohdecoded@gmail.com>
2019-08-14 18:50:50 -0400

    fixed login

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -10,45 +10,45 @@ mongoose.connect('mongodb://localhost/aBetterYou', {
   useCreateIndex: true,
 });
 
-const hashPassword = password =>
-  bcrypt.hash(password, SALT_WORK_FACTOR, (err, hash) => {
-    if (err) throw err;
-    return hash;
-  });
 
-const randyPassword = hashPassword('marshmallow');
-const doogiePassword = hashPassword('password');
+const randy = {
+  name: 'Randy K Mellow',
+  email: 'rsmellow@gmail.com',
+  password: 'marshmallow',
+  doctorPhoto:
+    'https://media.licdn.com/dms/image/C5603AQHZBYSHdmoC_A/profile-displayphoto-shrink_200_200/0?e=1539820800&v=beta&t=Qzy7Ukri7hgu3AZPn3pr9sE0ghcFSNy-8qcyGO8jkhw',
+  facilityName: 'UNC at Chapel Hill',
+  specialty: 'Head of JavaScript Development',
+  patients: [],
+  created: Date(Date.now()),
+};
 
-let doctorSeed = [
-  {
-    name: 'Randy K Mellow',
-    email: 'rsmellow@gmail.com',
-    password: randyPassword,
-    doctorPhoto:
-      'https://media.licdn.com/dms/image/C5603AQHZBYSHdmoC_A/profile-displayphoto-shrink_200_200/0?e=1539820800&v=beta&t=Qzy7Ukri7hgu3AZPn3pr9sE0ghcFSNy-8qcyGO8jkhw',
-    facilityName: 'UNC at Chapel Hill',
-    specialty: 'Head of JavaScript Development',
-    patients: [],
-    created: Date(Date.now()),
-  },
-  {
-    name: 'Doogie Houser, M.D.',
-    email: 'doogie@gmail.com',
-    password: doogiePassword,
-    doctorPhoto:
-      'http://mccurdywriting.com/wp-content/uploads/2010/09/Doogie_Howser_MD_290x400-290x300.jpg',
-    facilityName: 'Eastman Medical Center',
-    specialty: 'Child Genius Physician',
-    patients: [],
-    created: Date(Date.now()),
-  },
-];
-db.Doctor.deleteMany({})
-  .then(() => db.Doctor.collection.insertMany(doctorSeed))
-  .then(data => {
-    process.exit(0);
-  })
-  .catch(err => {
-    res.send(err);
-    process.exit(1);
+const doogie = {
+  name: 'Doogie Houser, M.D.',
+  email: 'doogie@gmail.com',
+  password: 'password',
+  doctorPhoto:
+    'http://mccurdywriting.com/wp-content/uploads/2010/09/Doogie_Howser_MD_290x400-290x300.jpg',
+  facilityName: 'Eastman Medical Center',
+  specialty: 'Child Genius Physician',
+  patients: [],
+  created: Date(Date.now()),
+};
+
+function seedDoctor(doctor) {
+  bcrypt.hash(doctor.password, SALT_WORK_FACTOR).then(hashedPassword => {
+    doctor.password = hashedPassword;
+    return db.Doctor.deleteMany({})
+      .then(() => db.Doctor.collection.insertOne(doctor))
+      .then(data => {
+        process.exit(0);
+      })
+      .catch(err => {
+        console.log(err);
+        process.exit(1);
+      });
   });
+}
+
+seedDoctor(randy);
+seedDoctor(doogie);
