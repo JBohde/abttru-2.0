dc58336173ddfa5ddd29f787c9d37eb8fccf7694
Joshua Bohde <bohdecoded@gmail.com>
2019-11-30 12:45:12 -0500

    feat: new production build

---
+++ client/build/precache-manifest.f19b40aea13b155f564bf73b190a75f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985fb554470b1d2ee6b39fc68842efa5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f742d523696fb2a0642",
+    "url": "/static/css/main.70e16c01.chunk.css"
+  },
+  {
+    "revision": "f08e9ecfb65a2b50b0cd",
+    "url": "/static/js/2.5ca3b33f.chunk.js"
+  },
+  {
+    "revision": "0f742d523696fb2a0642",
+    "url": "/static/js/main.4c9103f8.chunk.js"
+  },
+  {
+    "revision": "86dcf70707ba0fe14b11",
+    "url": "/static/js/runtime~main.9b6cb8e1.js"
+  },
+  {
+    "revision": "2a0e0e6aa9882889473d35639a0e1880",
+    "url": "/static/media/bitmojiAlberto.2a0e0e6a.png"
+  },
+  {
+    "revision": "a6471d8982a65415ab626d19889a4606",
+    "url": "/static/media/bitmojiDavey.a6471d89.png"
+  },
+  {
+    "revision": "72949bd847d7d9e0623ef8d81acc95d8",
+    "url": "/static/media/bitmojiJoshua2.72949bd8.png"
+  },
+  {
+    "revision": "fa0481f6d2fc19c9019ac8679266a368",
+    "url": "/static/media/icons8-no-entry-512.fa0481f6.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84a7bb8a00d25457fd4e21f7b035b749.js"
+  "/precache-manifest.f19b40aea13b155f564bf73b190a75f9.js"
 );
 
 self.addEventListener('message', (event) => {
