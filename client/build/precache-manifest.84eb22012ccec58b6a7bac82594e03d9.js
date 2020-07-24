38ce6f3cb36b3bebb5cb1d6fdc169ec6a06c8353
Joshua Bohde <bohdecoded@gmail.com>
2020-07-23 21:20:34 -0400

    feat: new build

---
+++ client/build/precache-manifest.84eb22012ccec58b6a7bac82594e03d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62bced0cf4c7752123d16af1402bc67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eec131d4e6686065eb7",
+    "url": "/static/css/main.04ba6073.chunk.css"
+  },
+  {
+    "revision": "f37b87c3efcb3c6ae392",
+    "url": "/static/js/2.91435150.chunk.js"
+  },
+  {
+    "revision": "1eec131d4e6686065eb7",
+    "url": "/static/js/main.a99d441a.chunk.js"
+  },
+  {
+    "revision": "988ebd2720b96f6cd1c9",
+    "url": "/static/js/runtime~main.9464611d.js"
+  },
+  {
+    "revision": "2a0e0e6aa9882889473d35639a0e1880",
+    "url": "/static/media/bitmojiAlberto.2a0e0e6a.png"
+  },
+  {
+    "revision": "a6471d8982a65415ab626d19889a4606",
+    "url": "/static/media/bitmojiDavey.a6471d89.png"
+  },
+  {
+    "revision": "72949bd847d7d9e0623ef8d81acc95d8",
+    "url": "/static/media/bitmojiJoshua2.72949bd8.png"
+  },
+  {
+    "revision": "fa0481f6d2fc19c9019ac8679266a368",
+    "url": "/static/media/icons8-no-entry-512.fa0481f6.png"
+  }
+]);
