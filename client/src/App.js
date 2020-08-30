9eadc032069c08e699707c6f7d33f9c58eb7f99b
Joshua Bohde <bohdecoded@gmail.com>
2020-08-30 13:15:39 -0400

    chore: minor css and eslint errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Doctor from './pages/Doctor';
 import DoctorLogin from './pages/Doctor/DoctorLogin';
-import CreateUser from './pages/Doctor/CreateUser';
-import EditUser from './pages/Doctor/EditUser';
-import UserInfo from './pages/Doctor/UserInfo';
+import CreatePatient from './pages/Doctor/CreatePatient';
+import EditPatient from './pages/Doctor/EditPatient';
+import PatientInfo from './pages/Doctor/PatientInfo';
 import User from './pages/User';
 import UserLogin from './pages/User/UserLogin';
 import SavedRecipes from './pages/User/SavedRecipes';
@@ -17,19 +17,19 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter keyLength={12}>
-        <div className="abttru">
+        <div className='abttru'>
           <NavigationBar />
-          <div className="main">
-            <Route exact path="/" component={Home} />
-            <Route path="/login/doctor" component={DoctorLogin} />
-            <Route path="/doctor/:id" component={Doctor} />
-            <Route path="/create/:id" component={CreateUser} />
-            <Route path="/edit/:id" component={EditUser} />
-            <Route path="/show/:id" component={UserInfo} />
-            <Route path="/login/user" component={UserLogin} />
-            <Route path="/user/:id" component={User} />
-            <Route path="/savedrecipes/:id" component={SavedRecipes} />
-            <Route path="/guest" component={Guest} />
+          <div className='main'>
+            <Route exact path='/' component={Home} />
+            <Route path='/login/doctor' component={DoctorLogin} />
+            <Route path='/doctor/:id' component={Doctor} />
+            <Route path='/create/:id' component={CreateUser} />
+            <Route path='/edit/:id' component={EditUser} />
+            <Route path='/show/:id' component={UserInfo} />
+            <Route path='/login/user' component={UserLogin} />
+            <Route path='/user/:id' component={User} />
+            <Route path='/savedrecipes/:id' component={SavedRecipes} />
+            <Route path='/guest' component={Guest} />
           </div>
           <Footer />
         </div>
