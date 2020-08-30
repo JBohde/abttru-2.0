c946f295128f262d099b866ab5ca511889af8a8d
Joshua Bohde <bohdecoded@gmail.com>
2020-08-30 13:04:45 -0400

    feat: add new icons for risk factor and logic to display properly according to risk

--- client/src/components/UserJumbotron/index.js
+++ client/src/components/UserJumbotron/index.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import {
-  Jumbotron, Container, Row, Col, Button, Media
-} from 'reactstrap';
+import { Jumbotron, Container, Row, Col, Button, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 
-import cholesterolIcon from './icons/cholesterol.png';
+import cholesterolIcon from './icons/high-cholesterol.png';
+import diabetesIcon from './icons/diabetes.png';
+import healthyIcon from './icons/healthy.png';
+import weightIcon from './icons/weight.png';
+
 import glucoseIcon from './icons/glucometer.png';
-import restrictionIcon from './icons/icons8-no-entry-512.png';
+import restrictionIcon from './icons/restriction.png';
 import pressureIcon from './icons/pressure.png';
 import recipeIcon from './icons/recipe.png';
 import tapeIcon from './icons/tape.png';
 import dietIcon from './icons/diet.png';
 import searchIcon from './icons/search.png';
 import './UserJumbotron.css';
+
+const determineRiskIcon = risk => {
+  switch (risk) {
+    case 'Healthy':
+      return healthyIcon;
+    case 'Obesity':
+      return weightIcon;
+    case 'Elevated Cholesterol':
+      return cholesterolIcon;
+    case 'Diabetic':
+      return diabetesIcon;
+    default:
+      return healthyIcon;
+  }
+};
 
 class UserJumbotron extends React.Component {
   constructor(props, context) {
@@ -27,7 +44,7 @@ class UserJumbotron extends React.Component {
       bpSystolic,
       bpDiastolic,
       healthLabel,
-      isUserPage
+      isUserPage,
     } = this.props;
 
     this.state = {
@@ -40,15 +57,16 @@ class UserJumbotron extends React.Component {
       dietRestriction: healthLabel,
       isUserPage,
       savedTab: '',
-      profileTab: ''
+      profileTab: '',
     };
   }
 
-  fontAwesomeColor = () => (this.props.riskFactor === 'high-cholesterol' ? 'red' : 'black');
+  fontAwesomeColor = () =>
+    this.props.riskFactor === 'high-cholesterol' ? 'red' : 'black';
 
-  profileTabColor = () => (this.props.isUserPage ? '#F3F0DD' : '#2C3E50');
+  profileTabColor = () => (this.props.isUserPage ? '#FAF8F0' : '#185477');
 
-  savedTabColor = () => (this.props.isUserPage ? '#2C3E50' : '#F3F0DD');
+  savedTabColor = () => (this.props.isUserPage ? '#185477' : '#FAF8F0');
 
   render() {
     const {
@@ -60,74 +78,87 @@ class UserJumbotron extends React.Component {
       bpSystolic,
       riskFactor,
       dietLabel,
-      dietRestriction
+      dietRestriction,
     } = this.props;
-
+    const riskIcon = determineRiskIcon(riskFactor);
     return (
-      <Jumbotron className="jumbo">
+      <Jumbotron>
         <Container>
-          <Row className="patient-profile">
+          <Row className='patient-profile'>
             <Col xs={12} md={4}>
-              <div className="user-image">
-                {userPhoto
-                  ? (<Media className="img-fluid" src={userPhoto} alt={`${firstName} ${lastName}`} />)
-                  : (<FontAwesomeIcon icon={faUser} className="user-icon" />)
-                }
+              <div className='user-image'>
+                {userPhoto ? (
+                  <Media
+                    className='img-fluid'
+                    src={userPhoto}
+                    alt={`${firstName} ${lastName}`}
+                  />
+                ) : (
+                  <FontAwesomeIcon icon={faUser} className='user-icon' />
+                )}
               </div>
             </Col>
             <Col xs={12} md={8}>
               <Row>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img
-                    src={cholesterolIcon}
-                    alt="cholesterol icon"
-                    className="health-icon"
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
+                    src={riskIcon}
+                    alt='risk icon'
+                    className='health-icon'
                   />
-                  <span className="health_stats">
+                  <span className='health_stats'>
                     Risk Factor: <br /> {riskFactor}
                   </span>
                 </Col>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
                     src={pressureIcon}
-                    alt="bp icon"
-                    className="health-icon"
+                    alt='bp icon'
+                    className='health-icon'
                   />
-                  <span className="health_stats">
+                  <span className='health_stats'>
                     Blood Pressure: <br />
                     {bpSystolic}
                   </span>
                 </Col>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
                     src={glucoseIcon}
-                    alt="glucose icon"
-                    className="health-icon"
+                    alt='glucose icon'
+                    className='health-icon'
                   />
-                  <span className="health_stats">
+                  <span className='health_stats'>
                     Blood Sugar: <br />
                     80 mg/dL
                   </span>
                 </Col>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img src={tapeIcon} alt="tape icon" className="health-icon" />
-                  <span className="health_stats">
-                    Waist: <br /> {waist}''
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
+                    src={tapeIcon}
+                    alt='tape icon'
+                    className='health-icon'
+                  />
+                  <span className='health_stats'>
+                    Waist: <br /> {waist}&nbsp;''
                   </span>
                 </Col>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img src={dietIcon} alt="diet icon" className="health-icon" />
-                  <span className="health_stats">
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
+                    src={dietIcon}
+                    alt='diet icon'
+                    className='health-icon'
+                  />
+                  <span className='health_stats'>
                     Diet: <br /> {dietLabel}
                   </span>
                 </Col>
-                <Col xs={6} md={4} className="stats-wrapper">
-                  <img
+                <Col xs={6} md={4} className='stats-wrapper'>
+                  <Media
                     src={restrictionIcon}
-                    alt="restriction icon"
-                    className="health-icon"
+                    alt='restriction icon'
+                    className='health-icon'
                   />
-                  <span className="health_stats">
+                  <span className='health_stats'>
                     Restriction: <br />
                     {dietRestriction && dietRestriction.length > 0
                       ? dietRestriction
@@ -138,15 +169,15 @@ class UserJumbotron extends React.Component {
             </Col>
           </Row>
         </Container>
-        <div className="tabGroup">
+        <div className='tabGroup'>
           <Link
             to={{
               pathname: `/user/${userId}`,
-              params: { id: userId }
+              params: { id: userId },
             }}
           >
             <Button
-              id="profileTab"
+              id='profileTab'
               style={{ backgroundColor: this.profileTabColor() }}
             >
               <p
@@ -154,10 +185,10 @@ class UserJumbotron extends React.Component {
                   color: this.savedTabColor(),
                   margin: '0.25rem 0',
                   fontSize: '.75rem',
-                  fontWeight: 'bold'
+                  fontWeight: 'bold',
                 }}
               >
-                <img src={searchIcon} alt="search icon" className="tab-icon" />
+                <img src={searchIcon} alt='search icon' className='tab-icon' />
                 New Search{' '}
               </p>
             </Button>
@@ -166,11 +197,11 @@ class UserJumbotron extends React.Component {
           <Link
             to={{
               pathname: `/savedrecipes/${userId}`,
-              params: { id: userId }
+              params: { id: userId },
             }}
           >
             <Button
-              id="savedTab"
+              id='savedTab'
               style={{ backgroundColor: this.savedTabColor() }}
             >
               <p
@@ -178,10 +209,10 @@ class UserJumbotron extends React.Component {
                   color: this.profileTabColor(),
                   margin: '0.25rem 0',
                   fontSize: '.75rem',
-                  fontWeight: 'bold'
+                  fontWeight: 'bold',
                 }}
               >
-                <img src={recipeIcon} alt="search icon" className="tab-icon" />{' '}
+                <img src={recipeIcon} alt='search icon' className='tab-icon' />{' '}
                 Saved Recipes
               </p>
             </Button>
