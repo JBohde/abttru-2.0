9ab03dba47454603437444bee5671fd9ecf6b36f
Joshua Bohde <bohdecoded@gmail.com>
2020-09-12 15:02:10 -0400

    fix: heroku deploy issues

--- server/server.js
+++ server/server.js
@@ -30,12 +30,14 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build/static'));
+  app.use(
+    '/static',
+    express.static(path.join(__dirname, '../client/build/static')),
+  );
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/'));
+  });
 }
-
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
 // Add API Routes
 app.use('/api', routes);
 
