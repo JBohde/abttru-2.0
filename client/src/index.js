59012703c9db22ecd407bd07b4ce6f029c96f861
Joshua Bohde <bohdecoded@gmail.com>
2020-09-12 12:22:55 -0400

    feat: add @fontawesom free-brand-svg-icons

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { fas }from "@fortawesome/free-solid-svg-icons";
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas);
+library.add(fas, fab);
 // Kicks off the process of finding <i> tags and replacing with <svg>
-dom.watch()
+dom.watch();
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
