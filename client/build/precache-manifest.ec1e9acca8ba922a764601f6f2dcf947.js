e470b615e219774e9612f9ac9c69fcac7b0daaac
Joshua Bohde <bohdecoded@gmail.com>
2020-09-12 12:54:54 -0400

    feat: production build 9.12.20

---
+++ client/build/precache-manifest.ec1e9acca8ba922a764601f6f2dcf947.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e626a1ce43bb8b2b98479a53588bc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80a3d70bf8555cedfe93",
+    "url": "/static/css/main.b91c049b.chunk.css"
+  },
+  {
+    "revision": "ce878173f5f50c475ae8",
+    "url": "/static/js/2.5df5f591.chunk.js"
+  },
+  {
+    "revision": "80a3d70bf8555cedfe93",
+    "url": "/static/js/main.18f2ea7a.chunk.js"
+  },
+  {
+    "revision": "988ebd2720b96f6cd1c9",
+    "url": "/static/js/runtime~main.9464611d.js"
+  },
+  {
+    "revision": "2a0e0e6aa9882889473d35639a0e1880",
+    "url": "/static/media/bitmojiAlberto.2a0e0e6a.png"
+  },
+  {
+    "revision": "a6471d8982a65415ab626d19889a4606",
+    "url": "/static/media/bitmojiDavey.a6471d89.png"
+  },
+  {
+    "revision": "72949bd847d7d9e0623ef8d81acc95d8",
+    "url": "/static/media/bitmojiJoshua2.72949bd8.png"
+  },
+  {
+    "revision": "c9ab5b352357c6abfdac3a1c3d4f23c0",
+    "url": "/static/media/diabetes.c9ab5b35.png"
+  },
+  {
+    "revision": "cd2f5cea12a37b7e8c102cb59301a102",
+    "url": "/static/media/healthy.cd2f5cea.png"
+  },
+  {
+    "revision": "0ce6b026bf6ad77f4244082daea4507f",
+    "url": "/static/media/high-cholesterol.0ce6b026.png"
+  },
+  {
+    "revision": "fa0481f6d2fc19c9019ac8679266a368",
+    "url": "/static/media/restriction.fa0481f6.png"
+  },
+  {
+    "revision": "f0cab457287c005c532c1edf9bca2874",
+    "url": "/static/media/weight.f0cab457.png"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84eb22012ccec58b6a7bac82594e03d9.js"
+  "/precache-manifest.ec1e9acca8ba922a764601f6f2dcf947.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
